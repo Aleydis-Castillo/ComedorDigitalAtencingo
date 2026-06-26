7fa64d9c8fe7dc363b267e7436ff380e309e986d
Aleydis-Castillo <aleydiscastillo187@gmail.com>
2026-06-26 13:01:19 -0500

    se agrego la pantalla de bienvenida y una gama de colores sugerida

---
+++ constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary: '#10ad5a',
+  secondary: '#165518',
+  lightGreen: '#85f5b0',
+  background: '#F5F7F5',
+  white: '#FFFFFF',
+  text: '#2F2F2F',
+  gray: '#6B7280',
+};
